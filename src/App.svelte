2a458a78886cea2d8a2fef36bac2ceb97006c14e
Cipher-Coder <matthew.rebehn@gmail.com>
2020-01-22 16:59:45 -0500

    initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,40 @@
+<script>
+  import "../node_modules/bulma/css/bulma.min.css";
+  import Nav from "./components/Nav.svelte";
+  import { Router, Link, Route } from "svelte-routing";
+
+  export let name;
+  name = "world";
+</script>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<Nav />
+<main>
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the
+    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
+</main>
---
+++ src/components/Nav.svelte
@@ -0,0 +1,49 @@
+<script>
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    const navbarBurgers = Array.prototype.slice.call(
+      document.querySelectorAll(".navbar-burger"),
+      0
+    );
+
+    if (navbarBurgers.length > 0) {
+      navbarBurgers.forEach(el => {
+        const target = el.dataset.target;
+        const target2 = document.getElementById(target);
+
+        el.classList.toggle("is-active");
+        target2.classList.toggle("is-active");
+      });
+    }
+  });
+</script>
+
+<style>
+  nav {
+    border-bottom: 1px solid rgba(255, 62, 0, 0.1);
+    padding: 0 1em;
+  }
+</style>
+
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="https://bulma.io">
+      <img
+        src="https://bulma.io/images/bulma-logo.png"
+        alt="Bulma: Free, open source, and modern CSS framework based on Flexbox"
+        width="112"
+        height="28" />
+    </a>
+
+    <span
+      role="button"
+      class="navbar-burger"
+      aria-label="menu"
+      aria-expanded="false">
+      <span aria-hidden="true" />
+      <span aria-hidden="true" />
+      <span aria-hidden="true" />
+    </span>
+  </div>
+</nav>
