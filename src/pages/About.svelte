37c474178b290f7f5b8f5b73d0183569bee1d5ab
Cipher-Coder <matthew.rebehn@gmail.com>
2020-01-24 17:29:31 -0500

    minor changes | migrating to sapper

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -9,6 +9,12 @@
 
   .microphone {
     border-radius: 6px;
+    -webkit-box-shadow: rgba(26, 26, 67, 0.05) 1px 1px 5px 0px,
+      rgba(50, 50, 93, 0.5) -39px 62.5px 125px -25px,
+      rgba(0, 0, 0, 0.3) 23.4px 37.5px 75px -37.5px;
+    box-shadow: rgba(26, 26, 67, 0.05) 1px 1px 5px 0px,
+      rgba(50, 50, 93, 0.5) -39px 62.5px 125px -25px,
+      rgba(0, 0, 0, 0.3) 23.4px 37.5px 75px -37.5px;
   }
 
   h1 {
@@ -23,7 +29,7 @@
   }
 
   .writing {
-    font-size: 1rem;
+    font-size: 1.1rem;
     line-height: 1.2rem;
   }
 
@@ -37,12 +43,15 @@
 
   .devtabs {
     margin: auto;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
     justify-content: center;
   }
 
   .devtabs-img {
     max-width: 800px;
     margin: auto;
+    border-radius: 16px;
   }
 
   .caption {
@@ -67,7 +76,21 @@
       <h1 class="is-centered">About Developer Podcasts</h1>
       <p class="writing">
         Podcasts have become a cultural phenomenon! They can inform, teach,
-        entertain, and much much more!
+        entertain, and much much more! Everyone loves them, including me!
+      </p>
+      <p class="writing">
+        The problem I encountered was there was no where I could go and just
+        type in "Developer Podcasts" and find them. Not just find them but find
+        the good ones. I searched and searched. Made my own list... but then I
+        had to spend the time listeneing to every single one, just to find out
+        which one was good. This has taken hours and I am still finding more and
+        more that I want to try out.
+      </p>
+      <p class="writing">
+        The solution... Developer Podcasts. Here you can submit podcasts that
+        you like, have listened to, and enjoyed. The community can rank them so
+        when anyone looks at the page they know immediately which podcasts are
+        great.
       </p>
     </div>
   </div>
@@ -75,12 +98,19 @@
     <h2>Check out some of my other projects:</h2>
   </div>
   <div class="devtabs columns">
-    <figure>
+    <figure class="figure-container">
       <img
         class="devtabs-img column"
         src="images/devtabs.png"
         alt="Screenshot of Dev Tabs" />
-      <figcaption class="caption">Dev Tabs Browser Extension</figcaption>
+      <figcaption class="caption">
+        <a
+          href="https://mattaz.com/devtabs"
+          target="_blank"
+          rel="noopener noreferrer">
+          Dev Tabs Browser Extension
+        </a>
+      </figcaption>
     </figure>
 
   </div>
